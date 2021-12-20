eaf94bc5d704e33b6368f4bcb33ce79d49dfd822
Matthew Peterson <matthew.se.peterson@gmail.com>
2021-12-20 17:26:24 -0500

    Finished solution to 14

--- 2021/rust/src/aoc/day14.rs
+++ 2021/rust/src/aoc/day14.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
 
-use std::fs;
-use std::path::Path;
-use std::io::{self, BufRead};
 use std::collections::HashMap;
+use std::fs;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 type Template = Vec<char>;
 type Rules = HashMap<(char, char), char>;
@@ -32,31 +32,49 @@ fn parse<P: AsRef<Path>>(path: P) -> io::Result<(Template, Rules)> {
 }
 
 pub fn solve_it() {
-    let (mut poly, rules) = parse("input/14.txt").unwrap();
+    let (poly, rules) = parse("input/14.txt").unwrap();
+    let mut pairs = HashMap::new();
 
-    for _ in 0..10 {
-        poly = poly
-            .iter()
-            .zip(poly[1..].iter())
-            .map(|(a, b)| {
-                if let Some(c) = rules.get(&(*a, *b)) {
-                    [*a, *c]
-                } else {
-                    [*a, *b]
+    // first and last characters are UNDERCOUNTED
+    let first = *poly.first().unwrap();
+    let last = *poly.last().unwrap();
+
+    for (a, b) in poly.iter().zip(poly[1..].iter()) {
+        *pairs.entry((*a, *b)).or_insert(0u64) += 1;
+    }
+
+    println!("{:?}", pairs);
+
+    for _ in 0..40 {
+        pairs = {
+            let mut new_pairs = pairs.clone();
+            for (a, b) in pairs.keys() {
+                let ab = (*a, *b);
+                if let Some(c) = rules.get(&ab) {
+                    let ac = (*a, *c);
+                    let cb = (*c, *b);
+                    *new_pairs.entry(ac).or_insert(0) += pairs[&ab];
+                    *new_pairs.entry(cb).or_insert(0) += pairs[&ab];
+                    *new_pairs.entry(ab).or_insert(pairs[&ab]) -= pairs[&ab];
                 }
-            })
-            .flatten()
-            .chain([*poly.last().unwrap()])
-            .collect();
+            }
+
+            new_pairs
+        }
     }
 
     let mut counts = HashMap::new();
-    for item in poly {
-        *counts.entry(item).or_insert(0) += 1;
+    counts.entry(first).or_insert(1u64);
+    counts.entry(last).or_insert(1);
+
+    for pair in pairs.keys() {
+        let (a, b) = *pair;
+        *counts.entry(a).or_insert(0) += pairs[pair];
+        *counts.entry(b).or_insert(0) += pairs[pair];
     }
 
     let min = counts.values().min().unwrap();
     let max = counts.values().max().unwrap();
 
-    println!("{}", max - min);
+    println!("{}", (max - min) / 2);
 }
